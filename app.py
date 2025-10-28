1a8486a3d899b9c0282b0ba801e29e25c5680ba2
Andrescjmaster <andresfelipeaguasaco@gmail.com>
2025-10-27 23:06:16 -0500

    correccion de pagina web

--- app.py
+++ app.py
@@ -289,6 +289,7 @@ def health():
 # ==============================================================
 # 🚀 MAIN
 # ==============================================================
-if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
